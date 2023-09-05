7bfe17e10a5806ccb558592b9d09d1167e3b25c0
Blackman37 <jan.cernousek@post.cz>
2023-09-05 11:08:27 +0200

    feat: basic route for create collection

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,53 @@ import * as Router from "koa-router"
 
 import * as logger from "koa-logger"
 import * as json from "koa-json"
+import * as bodyParser from "koa-bodyparser"
 
 import { getStatus } from "./helpers/get-status"
+import {
+  ICreateCollection,
+  createCollection,
+} from "./api/collections/create-collection/controller"
 
 const app = new Koa()
 const router = new Router()
 
 export const status = { status: "ok" }
 
+// Middlewares
+app.use(json())
+app.use(logger())
+app.use(bodyParser())
+
 // Hello world
-router.get("/", async (ctx, next) => {
+router.get("/status", async (ctx, next) => {
   ctx.body = getStatus()
 
   await next()
 })
 
-// Middlewares
-app.use(json())
-app.use(logger())
+// Create collection
+router.post("/collections", async (ctx, next) => {
+  console.log("test", ctx.request.body)
+  const body = <ICreateCollection>ctx.request.body
+
+  const { userId, name, stories } = body
+  const id = createCollection({
+    userId,
+    name,
+    stories,
+  })
+
+  ctx.body = { id }
+
+  await next()
+})
+
+router.get("/", async (ctx, next) => {
+  ctx.body = getStatus()
+
+  await next()
+})
 
 // Routes
 app.use(router.routes()).use(router.allowedMethods())
